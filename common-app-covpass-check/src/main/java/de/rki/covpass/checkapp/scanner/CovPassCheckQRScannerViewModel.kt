512600b31cfc72e599e869bb65b20b9e3e53f847
BurnPass <burnpass@example.com>
2023-10-25 00:12:35 +0200

    -CheckApp ignoriert Regelset sodass reduzierte Zertifikate sofort Gültig sind

--- common-app-covpass-check/src/main/java/de/rki/covpass/checkapp/scanner/CovPassCheckQRScannerViewModel.kt
+++ common-app-covpass-check/src/main/java/de/rki/covpass/checkapp/scanner/CovPassCheckQRScannerViewModel.kt
@@ -9,17 +9,15 @@ import com.ensody.reactivestate.BaseReactiveState
 import com.ensody.reactivestate.DependencyAccessor
 import com.ensody.reactivestate.ErrorEvents
 import com.ensody.reactivestate.MutableValueFlow
-import de.rki.covpass.checkapp.validitycheck.CovPassCheckValidationResult
-import de.rki.covpass.checkapp.validitycheck.validate
-import de.rki.covpass.commonapp.dependencies.commonDeps
-import de.rki.covpass.commonapp.storage.CheckContextRepository
 import de.rki.covpass.logging.Lumber
-import de.rki.covpass.sdk.cert.CovPassRulesValidator
 import de.rki.covpass.sdk.cert.QRCoder
-import de.rki.covpass.sdk.cert.models.*
+import de.rki.covpass.sdk.cert.models.CovCertificate
+import de.rki.covpass.sdk.cert.models.Recovery
+import de.rki.covpass.sdk.cert.models.TestCert
+import de.rki.covpass.sdk.cert.models.TestCertType
+import de.rki.covpass.sdk.cert.models.Vaccination
 import de.rki.covpass.sdk.cert.validateEntity
 import de.rki.covpass.sdk.dependencies.sdkDeps
-import de.rki.covpass.sdk.revocation.RevocationRemoteListRepository
 import kotlinx.coroutines.CoroutineScope
 import java.time.ZonedDateTime
 
@@ -39,10 +37,6 @@ internal interface CovPassCheckQRScannerEvents : ErrorEvents {
 internal class CovPassCheckQRScannerViewModel @OptIn(DependencyAccessor::class) constructor(
     scope: CoroutineScope,
     private val qrCoder: QRCoder = sdkDeps.qrCoder,
-    private val euRulesValidator: CovPassRulesValidator = sdkDeps.euRulesValidator,
-    private val domesticRulesValidator: CovPassRulesValidator = sdkDeps.domesticRulesValidator,
-    private val revocationRemoteListRepository: RevocationRemoteListRepository = sdkDeps.revocationRemoteListRepository,
-    private val checkContextRepository: CheckContextRepository = commonDeps.checkContextRepository,
 ) : BaseReactiveState<CovPassCheckQRScannerEvents>(scope) {
 
     val recoveryOlder90DaysValid: MutableValueFlow<Boolean> = MutableValueFlow(true)
@@ -53,38 +47,21 @@ internal class CovPassCheckQRScannerViewModel @OptIn(DependencyAccessor::class) 
                 val covCertificate = qrCoder.decodeCovCert(qrContent, is_covpass = false)
                 val dgcEntry = covCertificate.dgcEntry
                 validateEntity(dgcEntry.idWithoutPrefix)
-                when (
-                    validate(
-                        covCertificate,
-                        getRuleValidator(),
-                        revocationRemoteListRepository,
-                        recoveryOlder90DaysValid.value,
-                    )
-                ) {
-                    CovPassCheckValidationResult.Success -> {
-                        when (dgcEntry) {
-                            is Vaccination, is Recovery -> {
-                                eventNotifier {
-                                    onValidationSuccess(covCertificate)
-                                }
-                            }
-                            is TestCert -> {
-                                if (dgcEntry.type == TestCertType.NEGATIVE_PCR_TEST) {
-                                    handleNegativePcrResult(covCertificate)
-                                } else {
-                                    handleNegativeAntigenResult(covCertificate)
-                                }
-                            }
-                            // .let{} to enforce exhaustiveness
-                        }.let {}
+                when (dgcEntry) {
+                    is Vaccination, is Recovery -> {
+                        eventNotifier {
+                            onValidationSuccess(covCertificate)
+                        }
                     }
-                    CovPassCheckValidationResult.TechnicalError -> eventNotifier {
-                        onValidationFailure(true, covCertificate)
+                    is TestCert -> {
+                        if (dgcEntry.type == TestCertType.NEGATIVE_PCR_TEST) {
+                            handleNegativePcrResult(covCertificate)
+                        } else {
+                            handleNegativeAntigenResult(covCertificate)
+                        }
                     }
-                    CovPassCheckValidationResult.ValidationError -> eventNotifier {
-                        onValidationFailure(certificate = covCertificate)
-                    }
-                }
+                    // .let{} to enforce exhaustiveness
+                }.let {}
             } catch (exception: Exception) {
                 Lumber.e(exception)
                 eventNotifier { onValidationFailure(true) }
@@ -92,12 +69,6 @@ internal class CovPassCheckQRScannerViewModel @OptIn(DependencyAccessor::class) 
         }
     }
 
-    private fun getRuleValidator(): CovPassRulesValidator =
-        if (checkContextRepository.isDomesticRulesOn.value) {
-            domesticRulesValidator
-        } else {
-            euRulesValidator
-        }
 
     private fun handleNegativePcrResult(
         covCertificate: CovCertificate,
